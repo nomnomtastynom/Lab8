d610116538e743b20d56016a7580e45fd1f24f6d
nomnomtastynom <sarathsun123@gmail.com>
2019-03-26 16:05:45 -0500

    Added the TODO file

---
+++ src/Todo.java
@@ -0,0 +1,34 @@
+public class Todo {/*
+Lab Section_________: 11
+Last Modified_______: 2019-03-26
+OU ID_______________: 113428439
+OU EMAIL____________: sarath.c.sunkar-1@ou.edu
+
+Tasks:
+1. GamePiece Class
+2. GamePieceTest Class
+3. Finishing the largest class which is the BoardGame class
+4. Creating and properly executing the boardgame test class
+5. Learn what an enum is and use it for the color and shape classes 
+6. Learn how to test the enum classes and execute the tests 
+7. Finish the GamePieceAppearance method
+
+Estimated Time (minutes):
+1. 15
+2. 25
+3. 120
+4. 65
+5. 30
+6. 35
+7. 15
+
+Actual Time (minutes):
+1. 20
+2. 22
+3. 100
+4. 50
+5. 25
+6. 20
+7. 5
+*/
+    }
